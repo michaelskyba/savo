5ac909db0950d20dbb71cb74e29507173f62f651
Michael Skyba <michaelskyba@net-c.com>
2022-12-08 12:15:31 -0500

    make Sliders bounce back and forth

--- src/combat/Slider.ts
+++ src/combat/Slider.ts
@@ -7,11 +7,24 @@ class Slider extends Enemy {
 	constructor(y: number) {
 		super(-100, y, [0, 0], 0, "#111", "#eee")
 		this.counter = RNG(10, 15) * 2
-		this.cx = RNG(1, 3)/2
+		this.initSpeed(1)
+	}
+
+	initSpeed(direction: number) {
+		this.cx = RNG(1, 3) * direction
 	}
 
 	move() {
 		this.x += this.cx
+
+		if (this.cx > 0 && this.x > 1250) {
+			this.x = 1250
+			this.initSpeed(-1)
+		}
+		if (this.cx < 0 && this.x < 25) {
+			this.x = 25
+			this.initSpeed(1)
+		}
 	}
 }
 
